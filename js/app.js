5182f73c1707b4da4bb4740464030e7254611303
Neelabh Pandey <incore.labs.np@gmail.com>
2015-10-23 19:41:48 +0530

    fixed an issue when the orientation was changed
    This update fixes an issue where the chat-head went out of the visible
    area. now the chat-head is restored to the top of the page.

--- js/app.js
+++ js/app.js
@@ -1,16 +1,48 @@
-$(".chatHead")
-  .draggable({
-    containment: "document",
-    scroll: "false"
-  }).on("dragstop", function(event) {
-    var widthOfDocument = $(document).width();
-    if (event.clientX < (widthOfDocument / 2)) {
-      $(this).animate({
-        left: 0
-      });
-    } else {
-      $(this).animate({
-        left: widthOfDocument - $(this).width()
-      })
+var app = {
+  whichEdge: {
+    LEFT: 0,
+    RIGHT: 1
+  },
+  chatHeadPosition: null
+}
+
+$(document).ready(function() {
+  app.chatHeadPosition = app.whichEdge.RIGHT;
+  $(".chatHead")
+    .draggable({
+      containment: "document",
+      scroll: "false"
+    })
+    .on("dragstop", function(event) {
+      var widthOfBody = $(document.body).width();
+      if (event.clientX < (widthOfBody / 2)) {
+        app.chatHeadPosition = app.whichEdge.LEFT;
+        $(this).animate({
+          left: 0
+        });
+      } else {
+        app.chatHeadPosition = app.whichEdge.RIGHT;
+        $(this).animate({
+          left: widthOfBody - $(this).width()
+        })
+      }
+    });
+
+  $(window).resize(function() {
+    switch (app.chatHeadPosition) {
+      case app.whichEdge.LEFT:
+        $(".chatHead").css({
+          top: 0,
+          left: 0
+        });
+        break;
+      case app.whichEdge.RIGHT:
+        var widthOfBody = $(document.body).width();
+        $(".chatHead").css({
+          top: 0,
+          left: widthOfBody - $(".chatHead").width()
+        });
+        break;
     }
   });
+});
